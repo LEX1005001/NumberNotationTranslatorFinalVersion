15d7581881870fc8b5e53d6b706f8f2c971b78f7
LEX1005001 <AleX69.galkin@yandex.ru>
2024-01-10 21:22:52 +0300

    Unit Test (1/3)
    Unit Test Added

---
+++ dig2intUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dig2intUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dig2intUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3df8f5e4-9b46-4ed4-8256-705e9bf2ef71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ dig2intUnitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberTranslateLibrary;
+using System;
+
+namespace dig2intUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Необходимые переменые
+            const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";   //Алфавит систем счисления
+            char k = '0';                                                   //символ
+
+            int expected=digits.IndexOf(k);                                 //Ожидаемый результат
+            int result = ClassTranslate.dig2int(k);                         //Результат
+
+            Assert.AreEqual(expected, result);                              //Проверка 
+            
+
+
+        }
+    }
+}
